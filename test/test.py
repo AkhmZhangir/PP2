35dff9a7dae161ed1daae0413b64b57e21af28af
Zhangir <mistik31zhan@gmail.com>
2026-02-24 12:47:33 +0500

    3

--- test/test.py
+++ test/test.py
@@ -1,13 +1,19 @@
-b= {}
-a=int(input())
-for i in range(1,a+1):
-    c=input().split()
-    if len(c)==3:
-        b[c[1]]=c[2]
-    else:
-        if c[1] in b:
-            print(b[c[1]])
-        else:
-            print(f"KE: no key {c[1]} found in the document")
+class distance:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
+    def move(self, a, b):
+        self.a = a
+        self.b = b
+    def dist(self, a, b):
+        return ((self.a - a) ** 2 + (self.b - b) ** 2) ** 0.5
 
+x1, y1= map(int, input().split())
+print(f'({x1}, {y1})')
+point = distance(x1,y1)
+x2, y2= map(int, input().split())
+point.move(x2, y2)
+print(f'({x2}, {y2})')
+x3, y3= map(int, input().split())
+print(f'{point.dist(x3, y3):.2f}')
